e5b320f0eeff6aab18a75765c64f8d640d2edf05
Victor Daramola <49133983+daramolav1@users.noreply.github.com>
2022-09-22 14:44:07 -0400

    Time: 109 ms (18.72%), Space: 41.5 MB (94.23%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let count = 0
+    
+    while(num!==0){
+        if(num%2 === 0){
+            num/=2;
+            count++;
+        }
+        
+        if(num%2 !== 0){
+            num-=1;
+            count++
+        }
+    }
+    
+    return count
+};
